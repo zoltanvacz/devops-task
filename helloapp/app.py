07e51a3a355caf88358f7a1b89473bd964aa7d0b
zoltanvacz <vaczzoltan12@gmail.com>
2025-10-17 17:59:48 +0200

    Updated the python code with an error to test the Jenkins job

--- helloapp/app.py
+++ helloapp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hell, World!</p>"
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
